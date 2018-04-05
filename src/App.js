d3e8e25f763ace1a0670f2aed5df42e457aa023a
Taylor Boren <tboren@homeaway.com>
2018-04-05 12:15:06 -0500

    first real commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import data from './data.json';
+
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <h1>EMS Param Finder</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const mediaBaseUrl = '<media-assets-url>';
